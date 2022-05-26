3836f3522433f14845190eb1d7a2d3829557e6fb
Romulo-Ladeira <93687924+Romulo-Ladeira@users.noreply.github.com>
2022-05-26 18:51:47 -0300

    exercicio1

---
+++ ExercicioModulo1/Controllers/HelloWorldController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ExercicioModulo1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HelloWorldController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult hello()
+        {
+            return Ok("Hellow World");
+        }
+    }
+}
