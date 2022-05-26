8fadd4e59384d143047d9db0246b32fb7fdc4ced
Romulo-Ladeira <93687924+Romulo-Ladeira@users.noreply.github.com>
2022-05-26 19:24:33 -0300

    Exercicio2

---
+++ ExercicioModulo1/Entity/Produto.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ExercicioModulo1.Entity
+{
+    public class Produto
+    {
+        [Key]
+        [Required]
+        public int id { get; set; }
+        [Required]
+        public string nomeProduto { get; set; }
+        [Required]
+        public double valorProduto { get; set; }
+        [Required]
+        public DateTime dataCadastro { get; set; }
+        public DateTime dataCompra { get; set; }
+    }
+}
+    
